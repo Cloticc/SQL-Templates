a8ae4bb52dd6bfd8338bdc46f8d1fcd95d22a3df
Clotic <isidorssona@gmail.com>
2022-02-20 18:44:52 +0100

    Sql template
    A repo for sql templates:)

---
+++ Azerothcore/Npc/Npc_Template.sql
@@ -0,0 +1,9 @@
+SET
+-- can change entry to entry u want or keep it like this will pick highest entry + 1
+@Entry = (SELECT MAX(entry)+1 FROM creature_template),
+@Name = "XxX", -- name
+@SubName = "", -- can be left empty
+@Model = 25519, -- modelid
+@Faction = 35; -- change faction to your faction id
+
+INSERT INTO `acore_world`.`creature_template`(`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES (@Entry, 0, 0, 0, 0, 0, @Model, 0, 0, 0, @Name, @SubName, NULL, 0, 1, 1, 0, @Faction, 4227, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SQL-Templates
- 
+# [SQL-Templates]
+
+A repository for SQL templates.
+
+## Need help?
+
+If you encounter a bug please [open a issue](https://github.com/Cloticc/SQL-Templates/issues).
